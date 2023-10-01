782f9f48847330bb13b76f143073c5acce03b583
Roman Klasen <kontakt@romanklasen.de>
2023-10-01 15:42:07 +0200

    whoops, forgot to commit updated UseCase enum and FileReader

--- src/main/java/de/exxcellent/challenge/UseCase.java
+++ src/main/java/de/exxcellent/challenge/UseCase.java
@@ -1,5 +1,6 @@
 package de.exxcellent.challenge;
 
 public enum UseCase {
+    NOTEXISTING,
     WEATHER
 }
--- src/main/java/de/exxcellent/readers/FileReader.java
+++ src/main/java/de/exxcellent/readers/FileReader.java
@@ -1,5 +1,24 @@
 package de.exxcellent.readers;
 
-public abstract class FileReader implements DataSource{
-    
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public abstract class FileReader implements DataSource {
+    protected List<String> content;
+
+    public FileReader(String resourceURL) throws FileNotFoundException {
+        accessRessource(resourceURL);
+    }
+
+    public Boolean accessRessource(String resourceURL) throws FileNotFoundException {
+        try {
+            content = Files.readAllLines(Paths.get(resourceURL));
+            return true;
+        } catch (IOException e) {
+            throw new FileNotFoundException();
+        }
+    }
 }
